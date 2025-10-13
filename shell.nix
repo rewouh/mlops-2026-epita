eb5160bc893a6383e9599c76d27e94a6e62d61c1
Pierre Braud <pbraudpersonal@gmail.com>
2025-10-13 15:58:39 +0200

    TP6 start

--- shell.nix
+++ shell.nix
@@ -1,15 +1,29 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> {
+    config = {
+      cudaSupport = true;
+      allowUnfreePredicate = pkg:
+        builtins.elem (pkgs.lib.getName pkg) [
+          "cuda_cudart"
+          "libcublas"
+          "cuda_cccl"
+          "cuda_nvcc"
+        ];
+    };
+  }
+}:
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
         python311
-        python311Packages.numpy
         gcc
         glibc
+        glibc.bin
         libcxx
         iverilog
         gtkwave
   ];
 
-  LD_LIBRARY_PATH = "${pkgs.stdenv.cc.cc.lib}/lib";
+  shellHook = ''
+    export LD_LIBRARY_PATH=/run/opengl-driver/lib:${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
+  '';
 }
